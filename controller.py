dd4d8e4124cff4aa614deffcdeeed91856a04caa
Jack <ilsit@connect.ust.hk>
2026-04-25 10:13:40 +0800

    controller done

--- controller.py
+++ controller.py
@@ -1,15 +1,19 @@
 import torch
-
-
-
+import numpy as np
 
 
 # 根据四元数计算旋转矩阵
-def rotation_matrix(q0, q1, q2, q3) -> torch.Tensor:
-    _row0 = torch.Tensor([1-2*(q2**2)-2*(q3**2), 2*(q1*q2-q0*q3), 2*(q1*q3+q0*q2)])
-    _row1 = torch.Tensor([2*(q1*q2+q0*q3), 1-2*(q1**2)-2*(q3**2), 2*(q2*q3-q0*q1)])
-    _row2 = torch.Tensor([2*(q1*q3-q0*q2), 2*(q2*q3+q0*q1), 1-2*(q1**2)-2*(q2**2)])
-    return torch.Tensor((_row0, _row1, _row2))
+def rotation_matrix(q0, q1, q2, q3):
+    q0 = torch.as_tensor(q0)
+    q1 = torch.as_tensor(q1)
+    q2 = torch.as_tensor(q2)
+    q3 = torch.as_tensor(q3)
+    
+    _row0 = torch.stack([1-2*q2**2-2*q3**2, 2*(q1*q2-q0*q3), 2*(q1*q3+q0*q2)])
+    _row1 = torch.stack([2*(q1*q2+q0*q3), 1-2*q1**2-2*q3**2, 2*(q2*q3-q0*q1)])
+    _row2 = torch.stack([2*(q1*q3-q0*q2), 2*(q2*q3+q0*q1), 1-2*q1**2-2*q2**2])
+    
+    return torch.stack([_row0, _row1, _row2])
 
 
 def quat_multiply(q: torch.Tensor, p: torch.Tensor) -> torch.Tensor:
@@ -29,18 +33,12 @@ def quat_inverse(q: torch.Tensor) -> torch.Tensor:
     return torch.tensor([q[0], -q[1], -q[2], -q[3]])
 
 
-def attitude_error_quat(q_curr: torch.Tensor, q_des: torch.Tensor) -> torch.Tensor:
+def attitude_error_quat(q_curr_: torch.Tensor, q_des_: torch.Tensor) -> torch.Tensor:
     """
     Returns attitude error vector (3,) = 2 * vector part of (q_des^{-1} ⊗ q_curr)
     """
-    q_des_inv = quat_inverse(q_des)
-    q_error = quat_multiply(q_des_inv, q_curr)
+    if torch.dot(q_curr_, q_des_) < 0:
+        q_des_ = -q_des_
+    q_des_inv = quat_inverse(q_des_)
+    q_error = quat_multiply(q_des_inv, q_curr_)
     return 2.0 * q_error[1:]   # [x, y, z]
-
-
-# test pd controller
-def attitude_pd_control(q_curr, q_des, omega_curr, Kp_att, Kd_att):
-    att_err = attitude_error_quat(q_curr, q_des)
-    omega_err = omega_curr   # 因为期望角速度为 0
-    M = -Kp_att * att_err - Kd_att * omega_err
-    return M
--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from mujoco.glfw import glfw
 import torch
 import cv2
 import numpy as np
+import math
 
 import os
 os.environ["MUJOCO_GL"] = "glfw"
 
+from controller import attitude_error_quat, rotation_matrix, quat_multiply
 
-gravity = 9.0866        # m/s^2
-mass = 0.033            # kg
+
+g0 = 9.0866        # m/s^2
+mq = 0.033            # kg
 Ct = 3.25e-4            # N/krpm^2
 Cd = 7.9379e-6          # Nm/krpm^2
 arm_length = 0.065 / 2.0      # m
 max_thrust = 0.1573         # N
 max_torque = 3.842e-03      # Nm
+max_krpm = 22.0        # max krpm
 
 # 100Hz 10ms 0.01s
 dt_sim = 0.01
@@ -69,7 +73,7 @@ def control_callback(m, d):
             /            \\
     motor 4 CCW             motor 3  CW
     """
-    global log_count
+    global log_count, Ct, mq, g0, max_krpm
     _pos = d.qpos
     _vel = d.qvel
     _sensor_data = d.sensordata
@@ -83,25 +87,86 @@ def control_callback(m, d):
     quat_x = _sensor_data[7]
     quat_y = _sensor_data[8]
     quat_z = _sensor_data[9]
-    quat = torch.tensor([quat_x, quat_y, quat_z, quat_w])  # x y z w
+    quat = torch.tensor([quat_w, quat_x, quat_y, quat_z])  # w y x z
+    R_body_to_world = rotation_matrix(quat_w, quat_x, quat_y, quat_z)
     omega = torch.tensor([gyro_x, gyro_y, gyro_z])         # 角速度
+
     # 构建当前状态
-    current_state = torch.tensor([_pos[0], _pos[1], _pos[2], quat[3], quat[0], quat[1], quat[2], _vel[0], _vel[1], _vel[2], omega[0], omega[1], omega[2]])
+    # current_state = torch.tensor([_pos[0], _pos[1], _pos[2], quat[0], quat[1], quat[2], quat[3], _vel[0], _vel[1], _vel[2], omega[0], omega[1], omega[2]])
     # 位置控制模式 目标位点
     goal_position = torch.tensor([0.0, 0.0, 0.5])
 
-    # # NMPC Update
-    # _dt, _control = controller.nmpc_position_control(current_state, goal_position)
-    # d.actuator('motor1').ctrl[0] = calc_motor_input(_control[0])
-    # d.actuator('motor2').ctrl[0] = calc_motor_input(_control[1])
-    # d.actuator('motor3').ctrl[0] = calc_motor_input(_control[2])
-    # d.actuator('motor4').ctrl[0] = calc_motor_input(_control[3])
+    Kp_pos = 5.0   # 位置环比例增益，后面可调
+    v_des = Kp_pos * (goal_position - _pos[:3])
+    # v_des = torch.clamp(v_des, -2, 2)
 
-    # debug ctrl
-    d.actuator('motor1').ctrl[0] = calc_motor_input(15)
-    d.actuator('motor2').ctrl[0] = calc_motor_input(15)
-    d.actuator('motor3').ctrl[0] = calc_motor_input(15)
-    d.actuator('motor4').ctrl[0] = calc_motor_input(15)
+    Kv_p = 2.5     # 速度环比例增益
+    a_des = Kv_p * (v_des - _vel[:3]) + torch.tensor([0.0, 0.0, g0])
+    # g0 是重力加速度（正的，因为我们要抵消重力）
+
+    print(f"v_des: {v_des}, a_des: {a_des}")
+
+
+    z_curr = R_body_to_world[:, 2]   # 第三列
+    z_des = a_des / torch.norm(a_des)
+    axis = torch.linalg.cross(z_curr, z_des)
+    axis_norm = torch.norm(axis)
+    dot = torch.dot(z_curr, z_des)
+    # 避免数值问题
+    if axis_norm < 1e-6:
+        q_rot = torch.tensor([1.0, 0.0, 0.0, 0.0])
+    else:
+        angle = torch.atan2(axis_norm, dot)  # 更稳定
+        axis = axis / axis_norm
+        half_angle = angle * 0.5
+        q_rot = torch.tensor([torch.cos(half_angle), 
+                            axis[0]*torch.sin(half_angle),
+                            axis[1]*torch.sin(half_angle),
+                            axis[2]*torch.sin(half_angle)])
+
+    q_des = quat_multiply(q_rot, quat)
+    e_R = attitude_error_quat(q_curr_=quat, q_des_=q_des)
+    e_R[2] = 0.0 # free yaw
+
+    # att ctrl
+    # q_des = torch.tensor([1,0,0,0.5])
+    Kp_att = 10.0
+    omega_des = -Kp_att * e_R
+    # print(f"omega_des: {omega_des}, q_curr_: {quat}, q_des_: {q_des}")
+
+    Kd_rate = 0.001
+    M_des = Kd_rate * (omega_des - omega)
+    # print(f"M_des: {M_des}")
+
+    # M_des = torch.Tensor([0.0000,0.0000,0.0000])
+
+    # distrubute motors krpm
+    a_norm = torch.norm(a_des)
+    if a_norm < 1e-6:
+        F_des = mq * g0
+    else:
+        F_des = mq * a_norm
+    Mx, My, Mz = M_des[0], M_des[1], M_des[2]
+    # 系数
+    a = Ct          # 推力系数，注意单位与下面转速单位一致
+    b = Ct * arm_length      # 滚转/俯仰力矩系数
+    c = Cd          # 偏航力矩系数
+    # 解算四个电机转速平方（krpm²）
+    w1_sq = (F_des/a + Mx/b - My/b - Mz/c) / 4.0
+    w2_sq = (F_des/a - Mx/b - My/b + Mz/c) / 4.0
+    w3_sq = (F_des/a - Mx/b + My/b - Mz/c) / 4.0
+    w4_sq = (F_des/a + Mx/b + My/b + Mz/c) / 4.0
+    w1 = torch.sqrt(torch.clamp(w1_sq, min=0.0))
+    w2 = torch.sqrt(torch.clamp(w2_sq, min=0.0))
+    w3 = torch.sqrt(torch.clamp(w3_sq, min=0.0))
+    w4 = torch.sqrt(torch.clamp(w4_sq, min=0.0))
+    # hover_w = torch.sqrt(torch.tensor((mq*g0)/(4*Ct)))
+    # print("hover_w", hover_w)
+    # hovor speed: 15.777730167256925 krpm
+    d.actuator('motor1').ctrl[0] = calc_motor_input(torch.clamp(w1, 0, max_krpm))
+    d.actuator('motor2').ctrl[0] = calc_motor_input(torch.clamp(w2, 0, max_krpm))
+    d.actuator('motor3').ctrl[0] = calc_motor_input(torch.clamp(w3, 0, max_krpm))
+    d.actuator('motor4').ctrl[0] = calc_motor_input(torch.clamp(w4, 0, max_krpm))
 
     # 日志输出（每 50 次控制循环）
     log_count += 1
@@ -188,8 +253,8 @@ if __name__ == '__main__':
         mujoco.mjr_render(full_viewport, scene, context)
         mujoco.mjr_readPixels(full_rgb, full_depth, full_viewport, context)
         rgb_drone_camera = cv2.cvtColor(cv2.flip(full_rgb, 0), cv2.COLOR_RGB2BGR)
-        # cv2.imshow("Drone Camera 1920x1080", rgb_drone_camera)
-        # cv2.waitKey(1)
+        cv2.imshow("Drone Camera 1920x1080", rgb_drone_camera)
+        cv2.waitKey(1)
 
     cv2.destroyAllWindows()
     glfw.terminate()
